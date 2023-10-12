32556f3884e235f259c6a50596dbd08286c74cf7
Patryk Stefański <119447102+S4ter@users.noreply.github.com>
2023-10-12 13:19:55 +0200

    avatar saves in public avatars

---
+++ models/contacts.service.js
@@ -0,0 +1,65 @@
+const { Contact } = require("./contacts.model");
+
+const listContacts = async () => {
+  try {
+    return await Contact.find();
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+};
+
+const getContactById = async (contactId) => {
+  try {
+    return await Contact.findById(contactId);
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
+
+const removeContact = async (contactId) => {
+  try {
+    await Contact.findByIdAndDelete(contactId);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const addContact = async (contact) => {
+  try {
+    const newContact = new Contact(contact);
+    const saveContact = await newContact.save();
+    return saveContact;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const updateContact = async (contactId, body) => {
+  console.log(body);
+  try {
+    return await Contact.findByIdAndUpdate(contactId, body, { new: true });
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
+const updateStatusContact = async (contactId, favorite) => {
+  console.log(favorite);
+  try {
+    return await Contact.findByIdAndUpdate(contactId, favorite, { new: true });
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+  updateStatusContact,
+};
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -4,6 +4,7 @@ const {
   loginHandler,
   logoutHandler,
   currentHandler,
+  avatarHandler,
 } = require("./api/users.controller");
 const { userValidatorMiddleware } = require("../models/users/users.validators");
 const { authMiddleware } = require("../models/auth/auth.middleware");
@@ -14,6 +15,7 @@ usersRouter.post("/signup", userValidatorMiddleware, signupHandler);
 usersRouter.post("/login", loginHandler);
 usersRouter.post("/logout", authMiddleware, logoutHandler);
 usersRouter.get("/current", authMiddleware, currentHandler);
+usersRouter.patch("/avatars", authMiddleware, avatarHandler);
 
 module.exports = {
   usersRouter,
